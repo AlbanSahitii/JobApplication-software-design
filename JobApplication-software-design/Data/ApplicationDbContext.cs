f24c77ece5c1e4c224f5a66928fb9c49e097d270
Alban <Albansahiti2002@gmail.com>
2024-01-19 23:07:27 +0100

    Add project files.

---
+++ JobApplication-software-design/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobApplication_software_design.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
